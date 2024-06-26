7cccd09bdaf16b05aba36264b946f88c359a5971
Júlio César Neves <juliocnevess@gmail.com>
2024-06-25 21:24:04 -0300

    testa jpg

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
 
   eleventyConfig.addPassthroughCopy("*.ico");
+  eleventyConfig.addPassthroughCopy("*.jpg");
+  eleventyConfig.addPassthroughCopy("*.bin");
 };
